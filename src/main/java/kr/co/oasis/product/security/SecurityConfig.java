d805f793f17cbc126e238cd3efa3cc6d2644d108
JJONG <joshua_0621@naver.com>
2024-07-26 23:21:09 +0900

    [Style] 시큐리티 설정 줄바꿈 변경

--- src/main/java/kr/co/oasis/product/security/SecurityConfig.java
+++ src/main/java/kr/co/oasis/product/security/SecurityConfig.java
@@ -7,12 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
