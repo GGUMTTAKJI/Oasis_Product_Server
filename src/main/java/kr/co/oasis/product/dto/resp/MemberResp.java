9d28ba2c092fd79bd12a94e6fed4d82767dedede
JJONG <joshua_0621@naver.com>
2024-09-06 14:26:38 +0900

    [FIX] 로그인 응답 폼 수정

--- src/main/java/kr/co/oasis/product/dto/resp/MemberResp.java
+++ src/main/java/kr/co/oasis/product/dto/resp/MemberResp.java
@@ -1,7 +1,9 @@
 package kr.co.oasis.product.dto.resp;
 
+import kr.co.oasis.product.security.jwt.TokenDto;
+
 public record MemberResp() {
 
-    public record login(String status, String message, String token) {}
+    public record login(String status, String message, TokenDto token) {}
 
 }
